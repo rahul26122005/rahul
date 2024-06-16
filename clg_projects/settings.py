d050f2952486a8b3d208054fba4393e6b4f514df
rahul26122005 <rahulr26122005@gmail.com>
2024-06-16 19:01:51 +0530

    change the json and add the sh file

--- clg_projects/settings.py
+++ clg_projects/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0al@z@j5t#1&00fj49d3&kg%8ekzp*4rnt4rpl75*rs8hu&s^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- clg_projects/wsgi.py
+++ clg_projects/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clg_projects.settings')
 
 application = get_wsgi_application()
+
+app = application
